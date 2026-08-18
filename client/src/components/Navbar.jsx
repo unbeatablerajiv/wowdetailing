1ab43ea131043bd8fff6713ee4b2d72730e20f4d
Rajiv Singh <rajiv4som1@gmail.com>
2026-08-19 01:47:36 +0530

    Increase navigation logo size

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -29,7 +29,7 @@ export default function Navbar() {
         <div className="flex items-center justify-between h-16 md:h-20">
           {/* Logo */}
           <Link to="/" className="flex items-center">
-            <img src="/wow-logo.jpeg" alt="Wow Detailing" className="h-12 md:h-14 w-auto" />
+            <img src="/wow-logo.jpeg" alt="Wow Detailing" className="h-14 md:h-16 w-auto" />
           </Link>
 
           {/* Desktop nav */}
